3a72b810a04903b2898e524525b720843b31e4ab
Abhesh Mandal <abheshmandal249@gmail.com>
2024-08-28 23:36:11 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily : {
+        domine : ['Domine','serif']
+      }, 
+      animation : {
+        'rotating'  : 'rotating 5s linear infinite'
+      }, 
+      keyframes : {
+        rotating : {
+          '0%' : {transform : 'rotate(0.0deg)'}, 
+          '25%' : {transform : 'rotate(90deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          '75%' : {transform : 'rotate(deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          
+        }
+      }
+    },
+  },
+  plugins: [],
+}
